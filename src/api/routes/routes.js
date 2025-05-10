a7a387976819698822ce7e38356a4f349943ce9d
Haroun-Jinzo <jinzoharoun777@gmail.com>
2025-05-10 12:48:44 +0100

    Backend and Frontend

---
+++ src/api/routes/routes.js
@@ -0,0 +1,8 @@
+module.exports = app => {
+    const router = require('express').Router();
+    const postController= require('../controllers/post.controller');
+    router.post('/posts', postController.create)
+    router.get('/posts', postController.findAll)
+
+    app.use('/api/', router);
+}
---
+++ src/config/config.js
@@ -0,0 +1,3 @@
+require("dotenv").config();
+const config={DB_URL:process.env.DATABASE_URL}
+module.exports=config;
